7871e01983b17851355f295e6ae03ce3269dce61
Ankit Singh Rawat <83879728+asrawat2001@users.noreply.github.com>
2022-08-23 20:22:25 +0530

    changed host

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const botName = "ChatCord Bot";
 
 (async () => {
-  pubClient = createClient({ url: "redis://127.0.0.1:6379" });
+  pubClient = createClient({ url: "https://masterchatapp.herokuapp.com/" });
   await pubClient.connect();
   subClient = pubClient.duplicate();
   io.adapter(createAdapter(pubClient, subClient));
